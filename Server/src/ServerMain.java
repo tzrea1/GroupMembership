1b05484e2076084d3d376a94a6036f3cd37f3741
root <983884053@qq.com>
2022-12-19 03:44:09 +0800

    add a funtion that if deathTime=0,don't start GroupService

--- Server/src/ServerMain.java
+++ Server/src/ServerMain.java
@@ -16,15 +16,15 @@ public class ServerMain {
      **/
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String name=null;
+        String name = null;
         int portSelected = -1;
-        int deathTime=-1;
-        boolean is_introducer=false;
+        int deathTime = -1;
+        boolean is_introducer = false;
         while (name == null) {
             System.out.println("请输入虚拟机name:采用数字0、1、2、3....");
             // 接收输入的name
             name = sc.nextLine();
-            if (!(name.equals("0")||name.equals("1")||name.equals("2")||name.equals("3")||name.equals("4")||name.equals("5")||name.equals("6")||name.equals("7")||name.equals("8"))) {
+            if (!(name.equals("0") || name.equals("1") || name.equals("2") || name.equals("3") || name.equals("4") || name.equals("5") || name.equals("6") || name.equals("7") || name.equals("8"))) {
                 name = null;
                 System.out.println("请重新输入");
             }
@@ -43,13 +43,12 @@ public class ServerMain {
             System.out.println("是否将该虚拟机设为introducer?  yes：是；no：否");
             //接收输入的端口号
             String input = sc.nextLine();
-            if(input.equals("yes")){
+            if (input.equals("yes")) {
                 is_introducer = true;
             }
             if (input.equals("yes") || input.equals("no")) {
                 break;
-            }
-            else{
+            } else {
                 System.out.println("请重新输入");
             }
         }
@@ -58,7 +57,7 @@ public class ServerMain {
             // deathTime
             String inputStr = sc.nextLine();
             deathTime = Integer.parseInt(inputStr);
-            if (deathTime<=0) {
+            if (deathTime < 0) {
                 deathTime = -1;
                 System.out.println("请重新输入");
             }
@@ -66,7 +65,7 @@ public class ServerMain {
 
         // 创建查询虚拟机线程: portSelected
         int port = portList[portSelected];
-        VirtualServer vs = new VirtualServer(name,port + 100,is_introducer,deathTime);
+        VirtualServer vs = new VirtualServer(name, port + 100, is_introducer, deathTime);
         // 查询虚拟机线程
         Thread queryThread = new Thread(new Runnable() {
             public void run() {
@@ -77,15 +76,17 @@ public class ServerMain {
         queryThread.start();
         System.out.println("虚拟机已启动");
 
-        //在这里创建daemon的组服务// 组服务后台线程
-        Thread daemon = new Thread(new Runnable() {
-            public void run() {
-                vs.groupDaemon.startDaemon();
-            }
-        });
-        // 启动组服务后台线程
-        daemon.start();
-        System.out.println("组服务后台已启动");
+        if (deathTime!=0) {
+            //在这里创建daemon的组服务// 组服务后台线程
+            Thread daemon = new Thread(new Runnable() {
+                public void run() {
+                    vs.groupDaemon.startDaemon();
+                }
+            });
+            // 启动组服务后台线程
+            daemon.start();
+            System.out.println("组服务后台已启动");
+        }
 
         // 创建文件接收线程
         Initialize in = new Initialize();
